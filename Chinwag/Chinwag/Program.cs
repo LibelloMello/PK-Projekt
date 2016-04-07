b832e81c9a4e3856e66fc16aa50c2f8bfd7a5987
Gustav <gustav.jansater@gmail.com>
2016-04-07 12:41:19 +0200

    Start

---
+++ Chinwag/Chinwag/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinwag
+{
+    class User
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
