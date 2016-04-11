65ec0eee0e4edf422cb98869d5ba738c1ebd8eca
brnnmrk <kbrnnmrk@gmail.com>
2016-04-11 12:39:11 +0200

    Aint even mad

---
+++ Partycipate/Partycipate/DAL/Connector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Partycipate.DAL
+{
+    class Connector
+    {
+        public SqlConnection connection()
+        {
+
+            SqlConnection myConnection = new SqlConnection("user id=root;" +
+                                           "password=root;server=localhost;" +
+                                           "Trusted_Connection=yes;" +
+                                           "database=trappa; " +
+                                           "connection timeout=30");
+
+            Console.WriteLine("Connection");
+            Console.ReadLine();
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            return myConnection;
+
+        }
+    }
+}
