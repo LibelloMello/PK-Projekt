86c117ca662cf64fc9e3ec14ad1e58c41afc2ceb
Gustav <gustav.jansater@gmail.com>
2016-04-11 14:25:07 +0200

    HARAKIRI

---
+++ Partycipate/TestApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Partycipate;
+using System.Data.SqlClient;
+
+namespace TestApplication
+
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("innan vi skapar initierar DBCOnn");
+            DbUtil d = new DbUtil();
+
+            Console.WriteLine("Innan SQL");
+            SqlConnection myConnection = d.Connection();
+            if (myConnection != null)
+            {
+                SqlCommand myCommand = new SqlCommand("INSERT INTO dbo.USERS (USER_NAME, AGE, EMAIL, NAME, PASSWORD, PHONE_NBR, SEX) " +
+                                    "Values ('testunamn', 1955, 'otto@gmail.com', 'Test', 'testpw', '0999', 'Male')", myConnection);
+                myCommand.ExecuteNonQuery();
+                Console.WriteLine("Efter SQL");
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
+            d.closeConn(myConnection);
+            Console.Read();
+
+        }
+    }
+}
+
