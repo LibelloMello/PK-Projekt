2d54704153bc2c6e306590704eb4b97962d43179
Gustav <gustav.jansater@gmail.com>
2016-04-11 12:52:44 +0200

    Skapat DbUtil

--- Partycipate/Partycipate/DAL/UserAccess.cs
+++ Partycipate/Partycipate/DAL/UserAccess.cs
@@ -9,13 +9,16 @@ namespace Partycipate.DAL
 {
     class UserAccess
     {
-        public static FindUser(String userName)
+      /*  public static FindUser(String userName)
         {
             Connection con = null;
             SqlCommand myCom = new SqlCommand("SELECT u FROM User u WHERE userName = ?");
 
+        }*/
+        public static void CreateUser(String userName, int age, String email, String name, String password, String phoneNbr, String sex)
+        {
+            SqlCommand myCommand = new SqlCommand("INSERT INTO USERS VALUES ('testnamn', 1990, 'mail@test', 'testnamn', 'pwtest', '077test', 'Male", myConnection);
         }
-        public static CreateUser(String userName, )
 
 
 
