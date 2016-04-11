49691e5d9f11add75f5108663bed4ff1f39c5119
Gustav <gustav.jansater@gmail.com>
2016-04-11 14:21:03 +0200

    COMMIT SEPPUKU

--- Partycipate/Partycipate/DAL/UserAccess.cs
+++ Partycipate/Partycipate/DAL/UserAccess.cs
@@ -5,20 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Partycipate.DAL
+namespace DALE
 {
     class UserAccess
     {
-      /*  public static FindUser(String userName)
-        {
-            Connection con = null;
-            SqlCommand myCom = new SqlCommand("SELECT u FROM User u WHERE userName = ?");
+        /*  public static FindUser(String userName)
+          {
+              Connection con = null;
+              SqlCommand myCom = new SqlCommand("SELECT u FROM User u WHERE userName = ?");
 
-        }*/
+          }*/
         public static void CreateUser(String userName, int age, String email, String name, String password, String phoneNbr, String sex)
         {
-            SqlCommand myCommand = new SqlCommand("INSERT INTO USERS VALUES ('testnamn', 1990, 'mail@test', 'testnamn', 'pwtest', '077test', 'Male", myConnection);
-        }
+            try
+            {
+               // SqlConnection DbUtil.Connection(); 
+                //            SqlConnection myConnection = new SqlConnection();
+
+                SqlCommand myCommand = new SqlCommand("INSERT INTO USERS VALUES ('testnamn', 1990, 'mail@test', 'testnamn', 'pwtest', '077test', 'Male");
+            } catch(Exception e)
+            {
+
+            }
+            }
 
 
 
