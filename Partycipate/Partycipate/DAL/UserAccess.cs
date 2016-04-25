a42757ca221d6f09d16f96437ee2452f1ae1d324
brnnmrk <kbrnnmrk@gmail.com>
2016-04-25 11:57:00 +0200

    UserAccess update

--- Partycipate/Partycipate/DAL/UserAccess.cs
+++ Partycipate/Partycipate/DAL/UserAccess.cs
@@ -9,11 +9,11 @@ namespace Partycipate
 {
     public class UserAccess
     {
-       
+
         public User FindUser(string userName) //I GUI skall man skapa en text input field, sen döpa värdet man får
-                                                    //och skapa en string som sedan skickas med till denna metod
-                                                    // och sen returna SQLDataAdapter som kan håll SQLdata och sedan
-                                                    //loopa igenom för att skriva ut datan i en lista
+                                              //och skapa en string som sedan skickas med till denna metod
+                                              // och sen returna SQLDataAdapter som kan håll SQLdata och sedan
+                                              //loopa igenom för att skriva ut datan i en lista
         {
             DbUtil d = new DbUtil();
             Console.WriteLine("Innan SQL");
@@ -25,9 +25,9 @@ namespace Partycipate
             {
                 SqlCommand myCommand = new SqlCommand("SELECT * FROM USERS WHERE USER_NAME = @UserName", myConnection);
                 myCommand.Parameters.AddWithValue("@UserName", userName);
-                myReader = myCommand.ExecuteReader();            
+                myReader = myCommand.ExecuteReader();
 
-               
+
                 while (myReader.Read())
                 {
                     u.UserName = myReader["USER_NAME"].ToString();
@@ -44,8 +44,56 @@ namespace Partycipate
                 Console.WriteLine(e.ToString());
                 return null;
             }
+        }
+            public User CreateUser(string userName, string phoneNumber, string name, string email, string password, string sex, int age) {
 
-            public User createUser(string userName) { }
+            DbUtil d = new DbUtil();
+            Console.WriteLine("Innan SQL");
+            SqlConnection myConnection = d.Connection();
+            SqlDataReader myReader = null;
+            User u = new User();
+
+            try
+            {
+                SqlCommand myCommand = new SqlCommand("INSERT INTO PARTY VALUES(@UserName, @PhoneNumber, @Name, @Email, @Password, @Sex, @Age", myConnection);
+                myCommand.Parameters.AddWithValue("@UserName", userName);
+                myCommand.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                myCommand.Parameters.AddWithValue("@Name", name);
+                myCommand.Parameters.AddWithValue("@Email", email);
+                myCommand.Parameters.AddWithValue("@Password", password);
+                myCommand.Parameters.AddWithValue("@Sex", sex);
+                myCommand.Parameters.AddWithValue("@Age", age);
+                myReader = myCommand.ExecuteReader();
+
+
+                while (myReader.Read())
+                {
+                    u.UserName = myReader["USER_NAME"].ToString();
+                    u.PhoneNumber = myReader["PHONE_NUMBER"].ToString();
+                    u.Name = myReader["NAME"].ToString();
+                    u.Email = myReader["EMAIL"].ToString();
+                    u.Password = myReader["PASSWORD"].ToString();
+                    u.Sex = myReader["SEX"].ToString();
+                    u.Age = int.Parse(myReader["AGE"].ToString());
+                    Console.WriteLine(myReader["USER_NAME"].ToString());
+                    Console.WriteLine(myReader["PHONE_NUMBER"].ToString());
+                    Console.WriteLine(myReader["NAME"].ToString());
+                    Console.WriteLine(myReader["EMAIL"].ToString());
+                    Console.WriteLine(myReader["PASSWORD"].ToString());
+                    Console.WriteLine(myReader["SEX"].ToString());      
+                    Console.WriteLine(myReader["AGE"].ToString());
+
+                }
+                return u;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+
+        }
 
          
         }
