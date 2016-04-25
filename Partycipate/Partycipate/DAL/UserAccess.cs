4d28f3e41495426e53ad7335574ac8294b94a701
Gustav <gustav.jansater@gmail.com>
2016-04-25 14:23:21 +0200

    useraccess

--- Partycipate/Partycipate/DAL/UserAccess.cs
+++ Partycipate/Partycipate/DAL/UserAccess.cs
@@ -9,7 +9,7 @@ namespace Partycipate
 {
     public class UserAccess
     {
-      
+
 
         public User FindUser(string userName) //I GUI skall man skapa en text input field, sen döpa värdet man får
                                               //och skapa en string som sedan skickas med till denna metod
@@ -68,20 +68,6 @@ namespace Partycipate
                 myCommand.ExecuteNonQuery();
                 myConnection.Close();
 
-                /* while (myReader.Read())
-                 {
-                     u.UserName = myReader["USER_NAME"].ToString();
-                     u.PhoneNumber = myReader["PHONE_NUMBER"].ToString();
-                     u.Name = myReader["NAME"].ToString();
-                     u.Email = myReader["EMAIL"].ToString();
-                     u.Password = myReader["PASSWORD"].ToString();
-                     u.Sex = myReader["SEX"].ToString();
-                     u.Age = int.Parse(myReader["AGE"].ToString());
-
-            }
-               return u;
-                */
-               
             }
             catch (Exception e)
             {
@@ -90,57 +76,7 @@ namespace Partycipate
             }
 
         }
-        public void CreateUser2()
-        {
-
-            DbUtil d = new DbUtil();
-            SqlConnection myConnection = d.Connection();
-            SqlDataReader myReader = null;
-
-
-            try
-            {
-                string serName = "serName";
-                int honeNumber = 5432;
-                string ame = "ame";
-                string mail = "mail";
-                string assword = "assword";
-                string ex = "ex";
-                int ge = 92;
-                SqlCommand myCommand = new SqlCommand("INSERT INTO USERS VALUES(@UserName, @Age, @Name, @Email, @Password, @PhoneNumber, @Sex)", myConnection);
-                myCommand.Parameters.AddWithValue("@UserName", serName);
-                myCommand.Parameters.AddWithValue("@Age", ge);
-                myCommand.Parameters.AddWithValue("@Name", ame);
-                myCommand.Parameters.AddWithValue("@Email", mail);
-                myCommand.Parameters.AddWithValue("@Password", assword);
-                myCommand.Parameters.AddWithValue("@Sex", ex);
-                myCommand.Parameters.AddWithValue("@Age", ge);
-                myCommand.Parameters.AddWithValue("@PhoneNumber", honeNumber);
-                myCommand.ExecuteNonQuery();
-                myConnection.Close();
-
-                /* while (myReader.Read())
-                 {
-                     u.UserName = myReader["USER_NAME"].ToString();
-                     u.PhoneNumber = myReader["PHONE_NUMBER"].ToString();
-                     u.Name = myReader["NAME"].ToString();
-                     u.Email = myReader["EMAIL"].ToString();
-                     u.Password = myReader["PASSWORD"].ToString();
-                     u.Sex = myReader["SEX"].ToString();
-                     u.Age = int.Parse(myReader["AGE"].ToString());
-
-            }
-               return u;
-                */
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                return;
-            }
-
-        }
+        
 
 
     }
