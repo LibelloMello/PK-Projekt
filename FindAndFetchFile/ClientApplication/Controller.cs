6c550f6d94af423ed8310ddd0d4826af63a92fef
whereitat <erik.hallkvist@gmail.com>
2016-04-27 21:00:39 +0200

    The right files

---
+++ FindAndFetchFile/ClientApplication/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientApplication.FileServiceProvider;
+
+namespace ClientApplication
+{
+    class Controller
+    {
+        private FileService fs = new FileService();
+        public string ReadFile(string name)
+        {
+            return fs.ReadFile(name);
+        }
+    }
+}
---
+++ FindAndFetchFile/WebService/FileService.asmx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for FileService
+    /// </summary>
+    [WebService(Namespace = "http://grupp11.ics.lu.se/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class FileService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string ReadFile(string name)
+        {
+            string path = @"C:\\Users\Hallkvist\Desktop\MyFolder\";
+            if (!File.Exists(path + name) || !name.Contains(".txt"))
+            {
+                return "Couldn't fetch file. Error: File doesn't exist in MyFolder or isn't a text file. Troubleshooting: Make sure the file exists and enter file name followed by the extension '.txt'. Example: 'test.txt'.";
+            }
+            else
+            {
+                try
+                {
+                    using (var text = File.OpenText(path + name))
+                    {
+                        string line = text.ReadToEnd();
+                        return line;
+                    }
+                }
+                catch (SoapException soapEx)
+                {
+                    throw soapEx;
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw sqlEx;
+                }
+                catch (IOException ioex)
+                {
+                    throw ioex;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
+        }
+    }
+}
