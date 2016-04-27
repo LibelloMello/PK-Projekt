e2bb22239aa456eacf8a6fc18e7270cee9a705e6
whereitat <erik.hallkvist@gmail.com>
2016-04-27 23:41:10 +0200

    Console application updates ( NOT WSDL GENERATED APPLICATION )

--- FindAndFetchFile/ClientApplication/Program.cs
+++ FindAndFetchFile/ClientApplication/Program.cs
@@ -10,10 +10,26 @@ namespace ClientApplication
     {
         static void Main(string[] args)
         {
+            Start:
             Controller c = new Controller();
             Console.Write("In MyFolder, fetch file: ");
             Console.Write(c.ReadFile(Console.ReadLine()));
             Console.ReadKey();
+            Console.Clear();
+            
+            Console.WriteLine("Insert 'e' or 'exit' followed by Enter to exit application. Any other key to fetch a file!");
+            String input = Console.ReadLine();
+            if (string.Equals(input, "e", StringComparison.OrdinalIgnoreCase) || string.Equals(input, "exit", StringComparison.OrdinalIgnoreCase))
+            {
+                goto End;
+            }
+            else
+            {
+                goto Start;
+            }
+        End:
+            Environment.Exit(1);
+            
         }
     }
 }
