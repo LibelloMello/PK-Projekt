bfc20814822d7e0bad8ca1887a31f2859f805afd
whereitat <erik.hallkvist@gmail.com>
2016-04-28 13:57:25 +0200

    Controller mod

--- PartycipateWebService/ConsoleApplication/Controller.cs
+++ PartycipateWebService/ConsoleApplication/Controller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApplication.WebServiceReference;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace ConsoleApplication
 {
     class Controller
     {
+        WebServicePartycipateDB ws = new WebServicePartycipateDB();
+        public string GetUsers()
+        {
+            String output = "";
+            foreach (String s in ws.GetUsers())
+            {
+                output = output + s + " \n";
+            }
+            return output;
+        }
+        public string GetEvents()
+        {
+            String output = "";
+            foreach (String s in ws.GetEvents())
+            {
+                output = output + s + " \n";
+            }
+            return output;
+        }
     }
 }
