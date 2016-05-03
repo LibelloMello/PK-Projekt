dfad86f37cdf1f3e4c1e3cab7a69997bf0ce3826
whereitat <erik.hallkvist@gmail.com>
2016-05-03 15:52:40 +0200

    Name spave in WS changed

--- ERP_Service/ERPService/WebServiceERP.asmx.cs
+++ ERP_Service/ERPService/WebServiceERP.asmx.cs
@@ -10,7 +10,7 @@ namespace ERPService
     /// <summary>
     /// Summary description for WebServiceERP
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
+    [WebService(Namespace = "http://grupp11.ics.lu.se/ERP/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
