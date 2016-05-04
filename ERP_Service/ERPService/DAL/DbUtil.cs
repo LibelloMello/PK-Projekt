db4a1b1c4b9e588e2ddec668b880af963569c8e0
brnnmrk <kbrnnmrk@gmail.com>
2016-05-04 14:00:35 +0200

    SQL ERP

--- ERP_Service/ERPService/DAL/DbUtil.cs
+++ ERP_Service/ERPService/DAL/DbUtil.cs
@@ -18,16 +18,12 @@ namespace ERPService
                                            "database=Demo Database NAV (5-0); " +
                                            "connection timeout=30");
 
-            Console.WriteLine("I TRY Connection");
-
             try
             {
                 myConnection.Open();
-                Console.WriteLine("Success");
             }
             catch (Exception e)
             {
-                Console.WriteLine("I FAIL connection");
                 Console.WriteLine(e.ToString());
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
@@ -49,17 +45,86 @@ namespace ERPService
         }
 
 
-        public DataSet GetEmployees()
-        {
-            DbUtil db = new DbUtil();
-            SqlConnection myConnection = db.Connection();
-            DataSet ds = new DataSet();
-            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [CRONUS Sverige AB$Employee]", myConnection);
-            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
-            adapter.Fill(ds, "Employees");
+        /*  public DataSet GetEmployees()
+          {
+              DbUtil db = new DbUtil();
+              SqlConnection myConnection = db.Connection();
+              DataSet ds = new DataSet();
+              SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [CRONUS Sverige AB$Employee]", myConnection);
+              adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+              adapter.Fill(ds, "Employees");
 
-            return ds;
+              return ds;
+          }*/
+
+            // A
+        public List<List<string>> GetAllEmployees()
+        {
+            return ExecuteQuery("SELECT * FROM [CRONUS Sverige AB$Employee]");
         }
+
+        public List<List<string>> GetAllEmployeeRelatives()
+        {
+            return ExecuteQuery("SELECT * FROM [CRONUS Sverige AB$Employee Relative]");
+        }
+        public List<List<string>> GetAllSickEmployees2004()
+        {
+            return ExecuteQuery("SELECT * FROM [CRONUS Sverige AB$Employee Absence] WHERE Description = 'Sjuk' AND DATEPART(yy, [From Date]) = 2004");
+        }
+
+        public List<List<string>> GetEmployeesWithMostAbsence()
+        {
+            return ExecuteQuery("SELECT TOP 10 [First Name] " +
+                "FROM [CRONUS Sverige AB$Employee] E " +
+                "INNER JOIN " +
+                "[CRONUS Sverige AB$Employee Absence] A " +
+                "ON E.No_ = A.[Employee No_] " +
+                "WHERE Description = 'Sjuk' " +
+                "GROUP BY [First Name] " +
+                "HAVING COUNT([Entry No_]) > 1"
+            );
+        }
+
+        // B
+
+        public List<List<string>> GetCronusKeys()
+        {
+            return ExecuteQuery("SELECT NAME, TYPE_DESC FROM sys.key_constraints");
+        }
+
+        public List<List<string>> GetCronusIndexes()
+        {
+            return ExecuteQuery("SELECT OBJECT_ID, NAME FROM sys.indexes");
+        }
+
+        public List<List<string>> GetCronusTableConstraints()
+        {
+            return ExecuteQuery("SELECT TABLE_NAME, CONSTRAINT_TYPE FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS");
+        }
+
+        public List<List<string>> GetCronusTables()
+        {
+            return ExecuteQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES");
+        }
+    
+        public List<List<string>> GetCronusTables1()
+        {
+            return ExecuteQuery("SELECT NAME FROM sys.tables");
+        }
+
+        public List<List<string>> GetCronusEmployeeColumns()
+        {
+            return ExecuteQuery("SELECT COLUMN_NAME" +
+                "FROM INFORMATION_SCHEMA.COLUMNS " +
+                "WHERE TABLE_NAME = 'CRONUS Sverige AB$Employee'"
+            );
+        }
+        public List<List<string>> GetAllEmployeeColumns1()
+        {
+            return ExecuteQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'CRONUS Sverige AB$Employee'"
+            );
+        }
+
 
 
     }
